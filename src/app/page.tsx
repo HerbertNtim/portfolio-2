2f327b540e2298767677b333ddff685e660b6534
HerbertNtim <herbertntim15@gmail.com>
2024-08-19 19:40:37 +0000

    recent projects added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,18 @@
 import Hero from '@/components/Hero'
 import Grid from '@/components/Grid'
 import React from 'react'
+import RecentProjects from '@/components/RecentProjects'
+import { FloatingNav } from '@/components/ui/FloatingNavbar'
+import { navItems } from '@/data'
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
+      <FloatingNav navItems={navItems} />
         <Hero />
         <Grid />
+        <RecentProjects />
       </div>
     </main>
   )
