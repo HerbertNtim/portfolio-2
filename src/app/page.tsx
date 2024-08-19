748d8e669a4bbd1332cd992b98db391ab2388bd2
HerbertNtim <herbertntim15@gmail.com>
2024-08-19 18:44:38 +0000

    second section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Hero from '@/components/Hero'
+import Grid from '@/components/Grid'
 import React from 'react'
 
 const Home = () => {
@@ -6,6 +7,7 @@ const Home = () => {
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
         <Hero />
+        <Grid />
       </div>
     </main>
   )
