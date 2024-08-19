1ca20f0e18552efaeb5e1cf0291059aba0f6758e
HerbertNtim <herbertntim15@gmail.com>
2024-08-19 16:46:39 +0000

    hero done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import Hero from '@/components/Hero'
 import React from 'react'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
+    </main>
   )
 }
 
