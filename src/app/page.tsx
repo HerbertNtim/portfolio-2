f95c7f55f07df7d14fce5ae1c139978112ffe062
HerbertNtim <herbertntim15@gmail.com>
2024-08-20 16:29:17 +0000

    my approach

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import RecentProjects from '@/components/RecentProjects'
 import { FloatingNav } from '@/components/ui/FloatingNavbar'
 import { navItems } from '@/data'
 import Experience from '@/components/Experience'
+import Approach from '@/components/Approach'
 
 const Home = () => {
   return (
@@ -15,6 +16,7 @@ const Home = () => {
         <Grid />
         <RecentProjects />
         <Experience />
+        <Approach />
       </div>
     </main>
   )
