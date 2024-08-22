4fd2932d5fe31603ff29367263df0be338cd1c14
HerbertNtim <herbertntim15@gmail.com>
2024-08-22 01:55:24 +0000

    almost done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,10 +6,11 @@ import { FloatingNav } from '@/components/ui/FloatingNavbar'
 import { navItems } from '@/data'
 import Experience from '@/components/Experience'
 import Approach from '@/components/Approach'
+import Footer from '@/components/Footer'
 
 const Home = () => {
   return (
-    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+    <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-clip mx-auto sm:px-10 px-5">
       <div className="max-w-7xl w-full">
       <FloatingNav navItems={navItems} />
         <Hero />
@@ -17,6 +18,7 @@ const Home = () => {
         <RecentProjects />
         <Experience />
         <Approach />
+        <Footer />
       </div>
     </main>
   )
